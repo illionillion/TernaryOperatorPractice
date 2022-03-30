97cebedda13bd586c55361cdedff7fc03d53b874
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-03-31 00:22:30 +0900

    修正

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 
     let flag = true
     const ab = flag ? "A" : "B" //flagがtrueだったら"A"が入る、falseなら"B"
-    console.log(ab) // Aが出力される
+    console.log(ab)             // Aが出力される
 
     let randNum = Math.floor( Math.random() * 10 ) + 1 
     console.log(randNum)
@@ -11,7 +11,7 @@
     const odd2 = (randNum % 2 === 0) && "偶数" //trueの時だけ"偶数"、falseの時はfalseが入る
     console.log(odd)
     console.log(odd2)
-    console.log(odd2 || "奇数") //もしodd2がfalse(奇数)だった場合
+    console.log(odd2 || "奇数")                //もしodd2がfalse(奇数)だった場合
 
     //即時関数内でもloadとかしないと要素は取得できない
     // window.addEventListener('load', e =>{

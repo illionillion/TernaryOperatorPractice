33f062491138996964429d589b611b22c8ce6b06
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-03-30 22:53:35 +0900

    少し書き換え

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 "use strict";
 (()=>{
+
     let flag = true
     const ab = flag ? "A" : "B" //flagがtrueだったら"A"が入る、falseなら"B"
-    console.log(ab)
+    console.log(ab) // Aが出力される
 
-    let num = Math.floor(Math.random() * 10) + 1 
-    console.log(num)
-    const odd = (num%2==0) ? "偶数" : "奇数" 
-    const odd2 = (num%2==0) && "偶数" //trueの時だけ
+    let randNum = Math.floor( Math.random() * 10 ) + 1 
+    console.log(randNum)
+    const odd = (randNum % 2 === 0) ? "偶数" : "奇数" 
+    const odd2 = (randNum % 2 === 0) && "偶数" //trueの時だけ"偶数"、falseの時はfalseが入る
     console.log(odd)
     console.log(odd2)
-
-    
+    console.log(odd2 || "奇数") //もしodd2がfalse(奇数)だった場合
 
     //即時関数内でもloadとかしないと要素は取得できない
     // window.addEventListener('load', e =>{
